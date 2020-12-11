7fa55bfb30e362892741e695e420f86c33e0c7d9
gioianm <32346756+gioianm@users.noreply.github.com>
2020-12-11 20:16:18 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -3,19 +3,21 @@
 var lastScrollPosition = 0; 
 
 $('#scroll-up').click( function(){
-  if ( $(document).scrollTop() > 0 ) {
-    $('body').animate({scrollTop:0},1000);
-    lastScrollPosition = $(document).scrollTop();
-  } else {
-    $('body').animate({scrollTop:lastScrollPosition},1000);
-  } 
+    if ( $(document).scrollTop() > 0 ) {
+        $('html').animate({scrollTop:0},1000);
+        $('body').animate({scrollTop:0},1000);
+        lastScrollPosition = $(document).scrollTop();
+    } else {
+        $('html').animate({scrollTop:lastScrollPosition},1000);
+        $('body').animate({scrollTop:lastScrollPosition},1000);
+    }   
 });
 
 $(document).scroll( function() {
-  if ( $(document).scrollTop() > 0 ) {
-    $('#scroll-up').fadeIn();
-    $('#scroll-up').text('Наверх');
-  } else {
-    $('#scroll-up').text('Вниз');
-  }
+    if ( $(document).scrollTop() > 0 ) {
+        $('#scroll-up').fadeIn();
+        $('#scroll-up').text('Up');
+    } else {
+        $('#scroll-up').text('Down');
+    }
 });
